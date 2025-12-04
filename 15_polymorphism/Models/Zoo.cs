cf5118a4f63aaf2147145485dd66435693ff4cd3
Ali ahmadi <aliahmadi8822@gmail.com>
2025-12-04 16:48:10 +0100

    update

--- 15_polymorphism/Models/Zoo.cs
+++ 15_polymorphism/Models/Zoo.cs
@@ -5,6 +5,29 @@ public class Zoo
     public List<Animal> ListOfAnimal { get; set; } = new List<Animal>();
     public string Name { get; set; }
 
+    public Zoo(Zoo original)
+    {
+        Name = original.Name;
+        ListOfAnimal = new List<Animal>();
+
+        // Deep copy: create new animal instances using their respective copy constructors
+        foreach (var animal in original.ListOfAnimal)
+        {
+            Animal newAnimal = animal switch
+            {
+                NordicAnimal nordic => new NordicAnimal(nordic),    // Calls NordicAnimal copy constructor
+                AfricanAnimal african => new AfricanAnimal(african),  // Calls AfricanAnimal copy constructor
+                HunterBird bird => new HunterBird(bird),             // Calls HunterBird copy constructor
+                _ => new Animal(animal)                              // Calls Animal copy constructor
+            };
+            ListOfAnimal.Add(newAnimal);
+        }
+    }
+
+    public Zoo()
+    {
+    }
+
     public override string ToString()
     {
         string sRet = $"\nAnimals in Zoo {Name}:";
--- 15_polymorphism/Program.cs
+++ 15_polymorphism/Program.cs
@@ -57,6 +57,56 @@ internal class Program
         zoo1.ReseedAllAnimals(seeder);
         Console.WriteLine(zoo1.ToString());
 
+        var originalZoo = new Zoo() { Name = "City Zoo" };
+
+        originalZoo.ListOfAnimal.AddRange(seeder.ItemsToList<NordicAnimal>(5));
+
+        var shallowCopiedZoo = new Zoo(originalZoo);
+
+        shallowCopiedZoo.Name = "Shallow Copied Zoo";
+
+        shallowCopiedZoo.ListOfAnimal.AddRange(seeder.ItemsToList<AfricanAnimal>(2));
+        Console.WriteLine();
+
+        Console.WriteLine("Original zoo name: " + " " + originalZoo.Name);
+        Console.WriteLine("Original zoo animal count: " + originalZoo.ListOfAnimal.Count);
+
+        Console.WriteLine("Shallow copied zoo name: " + " " + shallowCopiedZoo.Name);
+        Console.WriteLine("Shallow copied zoo animal count: " + shallowCopiedZoo.ListOfAnimal.Count);
+
+
+        var originalZoo1 = new Zoo() { Name = "National Zoo" };
+
+        originalZoo1.ListOfAnimal.AddRange(seeder.ItemsToList<NordicAnimal>(5));
+
+        var shallowCopiedZoo1 = new Zoo(originalZoo1);
+
+        shallowCopiedZoo1.Name = "shallow copy zoo1";
+        shallowCopiedZoo1.ListOfAnimal.AddRange(seeder.ItemsToList<NordicAnimal>(2));
+        shallowCopiedZoo1.ListOfAnimal[0].Name = "Modify animal name";
+
+        Console.WriteLine(originalZoo1.ToString());
+        Console.WriteLine("animal count: " + originalZoo1.ListOfAnimal.Count);
+        Console.WriteLine(shallowCopiedZoo1.ToString());
+        Console.WriteLine("animal count: " + shallowCopiedZoo1.ListOfAnimal.Count);
+
+
+        var originalzoo2 = new Zoo() { Name = "city zoo" };
+
+        originalzoo2.ListOfAnimal.AddRange(seeder.ItemsToList<NordicAnimal>(5));
+
+        var deepcopy = new Zoo(originalzoo2);
+        deepcopy.Name = "Deep copied zoo";
+        deepcopy.ListOfAnimal.AddRange(seeder.ItemsToList<NordicAnimal>(2));
+        deepcopy.ListOfAnimal[0].Name = "deep copy animal";
+        deepcopy.ListOfAnimal[0].Age = 99;
+
+        Console.WriteLine(originalzoo2.ToString());
+        Console.WriteLine("Animal count: " + originalzoo2.ListOfAnimal.Count);
+        Console.WriteLine(deepcopy.ToString());
+        Console.WriteLine("Animal count: " + deepcopy.ListOfAnimal.Count);
+
+
 
 
     }
